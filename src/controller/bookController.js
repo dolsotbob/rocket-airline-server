45f52653dfdbb1e6140004df9b4fa1769a43237e
dolsotbob <1hyoon3@naver.com>
2025-05-12 17:44:56 +0900

    task completed

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -1,13 +1,37 @@
 const flights = require('../repository/flightList');
-// 항공편 예약 데이터를 저장합니다.
+// (서버가 실행되는 동안 메모리에) 항공편 예약 데이터를 저장합니다.
 let booking = [];
 
 module.exports = {
   // [GET] /book 요청을 수행합니다.
-  // 전체 데이터 혹은 요청 된 flight_uuid, phone 값과 동일한 예약 데이터를 조회합니다.
+  // 기능: 요청된 flight_uuid 또는 phone 쿼리값으로 예약 조회
+  // 아무 조건 없으면 전체 예약 조회 
   findById: (req, res) => {
     // TODO:
-    return res.status(200).json('not implemented');
+    // console.log(req);
+
+    const { flight_uuid, phone } = req.query;
+
+    if (flight_uuid) {
+      const filtered = booking.filter((b) => b.flight_uuid === flight_uuid);
+      if (filtered.length === 0) {
+        return res.status(404).json({ message: 'Booking not Found' });
+      }
+
+      return res.status(200).json(filtered);
+    }
+
+    if (phone) {
+      const found = booking.filter((b) => b.phone === phone);
+      if (found.length === 0) {
+        return res.status(404).json({ message: 'Booking not Found ' });
+      }
+
+      return res.status(200).json(found[0]);
+    }
+
+    // 전체 데이터 
+    return res.status(200).json(booking);
   },
 
   // [POST] /book 요청을 수행합니다.
@@ -17,13 +41,33 @@ module.exports = {
   // 참고 링크: https://www.vinaysahni.com/best-practices-for-a-pragmatic-restful-api#useful-post-responses
   create: (req, res) => {
     // TODO:
-    return res.status(201).json({});
+    // console.log(req.body);
+    // 구조 분해 하는 것으로 시작 
+    const { flight_uuid, name, phone } = req.body;
+    // const booking = { flight_uuid, name, phone };
+    if (!flight_uuid || !name || !phone) {
+      return res.status(400).json({ message: 'Bad Request' });
+    }
+
+    const newBooking = { flight_uuid, name, phone };
+    booking.push(newBooking);
+
+    // Location Header로 예약 아이디를 함께 보내준다면 RESTful한 응답
+    res.location(`/book/${flight_uuid}`);
+
+    // 응답으로는 book_id를 리턴
+    // 200이 아닌 201임  
+    return res.status(201).json({ book_id: flight_uuid });
   },
 
   // [DELETE] /book?phone={phone} 요청을 수행합니다.
   // 요청 된 phone 값과 동일한 예약 데이터를 삭제합니다.
   deleteById: (req, res) => {
     // TODO:
-    return res.status(200).json('not implemented');
+    const { phone } = req.query;
+
+    // 요청 된 phone 값과 동일한 예약 데이터를 삭제한다 
+    booking = booking.filter((b) => b.phone !== phone);
+    return res.status(200).json(booking);
   },
 };
--- src/controller/flightController.js
+++ src/controller/flightController.js
@@ -4,14 +4,39 @@ module.exports = {
   // [GET] /flight
   // 요청 된 departure_times, arrival_times, destination, departure 값과 동일한 값을 가진 항공편 데이터를 조회합니다.
   findAll: (req, res) => {
+    // console.log(req);
     // TODO:
-    return res.status(200).send('not implemented');
+    try {
+      const { departure_times, arrival_times, destination, departure } = req.query;
+
+      const filtered = flights.filter((flight) => {
+        return (!departure || flight.departure === departure) &&
+          (!destination || flight.destination === destination) &&
+          (!departure_times || flight.departure_times === departure_times) &&
+          (!arrival_times || flight.arrival_times === arrival_times);
+      });
+
+      return res.status(200).json(filtered);
+    } catch (error) {
+      return res.status(500).json({ message: 'Internal Server Error' })
+    }
   },
   // [GET] /flight/:id
   // 요청 된 id 값과 동일한 uuid 값을 가진 항공편 데이터를 조회합니다.
   findById: (req, res) => {
     // TODO:
-    return res.status(200).json('not implemented');
+    try {
+      // f는 flightList 배열에서 하나씩 꺼낸 항공편 객체 
+      // req.params.id: URL 경로에 포함된 :id 값을 가져옴 
+      // find()는 자바스크립트의 내장함수; 배열에서 조건을 만족ㅎ는 첫 번째 요소를 찾아 반환함
+      // console.log(req);
+      const flight = flights.find((f) => f.uuid === req.params.id);
+      if (!flight) return res.status(404).json({ message: 'Flight not found' });
+      // 항공편이 존재하면 [배열]에 담아 반환; 배열로 감싸는 이유는 테스트 코드가 res.body[0] 방식으로 응답을 확인하기 때문
+      return res.status(200).json([flight]);
+    } catch (error) {
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
   },
 
   // [PUT] /flight/:id 요청을 수행합니다.
@@ -19,7 +44,18 @@ module.exports = {
   update: (req, res) => {
     let data;
     // TODO:
+    // console.log(req.body);
+    // console.log(req.params.id);
+    try {
+      const { id } = req.params;
+      const index = flights.findIndex((f) => f.uuid === id);
+      if (index === -1) return res.status(404).json({ message: 'Flight not fount' });
 
-    return res.status(200).json(data);
+      flights[index] = { ...flights[index], ...req.body };
+      data = flights[index];
+      return res.status(200).json(flights[index]);
+    } catch (error) {
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
   },
 };
